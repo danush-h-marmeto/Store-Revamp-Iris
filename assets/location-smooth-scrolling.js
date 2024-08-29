0ac2c1aa0d2c8e67443216f348b42afb805cfed8
danush07 <danush.h@marmeto.com>
2024-08-29 17:09:55 +0530

    adding smooth scroll and the window screen triggers

--- assets/location-smooth-scrolling.js
+++ assets/location-smooth-scrolling.js
@@ -1,10 +1,13 @@
 class SmoothScrollElement extends HTMLElement {
   constructor() {
     super();
+    this.isScrolling = false;
+    this.scrollTimeout = null;
   }
 
   connectedCallback() {
     this.addEventListeners();
+    this.observeTargetElements();
   }
 
   addEventListeners() {
@@ -19,11 +22,63 @@ class SmoothScrollElement extends HTMLElement {
     const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
+      this.isScrolling = true;
       window.scrollTo({
         top: targetElement.offsetTop - 200,
         behavior: "smooth",
       });
+      this.querySelectorAll(".grid__item").forEach((item) => {
+        item.classList.remove("active");
+      });
+      event.currentTarget.classList.add("active");
+      this.checkScrollEnd();
     }
   }
+
+  checkScrollEnd() {
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+
+    this.scrollTimeout = setTimeout(() => {
+      requestAnimationFrame(() => {
+        this.isScrolling = false;
+      });
+    }, 2000); 
+  }
+
+  observeTargetElements() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (!this.isScrolling) {
+          entries.forEach((entry) => {
+            const targetElement = entry.target;
+            const targetId = targetElement.id;
+            const gridItem = this.querySelector(`[href="#${targetId}"]`);
+
+            if (entry.isIntersecting) {
+              gridItem.classList.add("active");
+            } else {
+              gridItem.classList.remove("active");
+            }
+          });
+        }
+      },
+      {
+        root: null,
+        threshold: 1, 
+      }
+    );
+
+    this.querySelectorAll(".grid__item").forEach((item) => {
+      const targetId = item.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        observer.observe(targetElement);
+      }
+    });
+  }
 }
+
 customElements.define("smooth-scroll-element", SmoothScrollElement);
